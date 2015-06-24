63d7da7125fa7c86ece434486a2c45e62ac4d76a
Loren Hale <loren.hale@gmail.com>
2015-06-24 15:53:05 +0800

    add NumToWord class

---
+++ lib/num_to_word.rb
@@ -0,0 +1,16 @@
+class NumToWord
+
+  def initialize number
+    @number = number
+    @string_name = parse
+  end
+
+  def to_string
+    @string_name
+  end
+
+  private
+
+  def parse
+  end
+end
