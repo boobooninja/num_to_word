f8a15eb14c65d85bb2b196cbd50649cddb669441
Loren Hale <loren.hale@gmail.com>
2015-06-24 15:50:30 +0800

    test NumToWord class initialization

--- spec/num_to_word_spec.rb
+++ spec/num_to_word_spec.rb
@@ -1,2 +1,19 @@
 require 'spec_helper'
-require 'NumToWord'
+require 'num_to_word'
+
+describe NumToWord do
+
+  before do
+    @instance = NumToWord.new(123)
+  end
+
+  describe 'new' do
+    it 'should accept one argument' do
+      expect(NumToWord).to respond_to(:new).with(1).argument
+    end
+
+    it 'should return an object of class NumToWord' do
+      expect(@instance).to be_a(NumToWord)
+    end
+  end
+end
