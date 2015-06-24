eca317dba1826bfa9f02e2017b8ba861d6406417
Loren Hale <loren.hale@gmail.com>
2015-06-24 16:18:17 +0800

    add tests
    * check for long numbers
    * test to_string output

--- spec/num_to_word_spec.rb
+++ spec/num_to_word_spec.rb
@@ -21,14 +21,28 @@ describe NumToWord do
       expect(@instance.instance_variable_get(:@string_name)).to eq('one hundred twenty three')
     end
 
-    it 'should remove any commas from the number' do
-      expect(NumToWord.new('1,000').instance_variable_get(:@string_number)).to eq('1000')
+    context 'when number is a string and contains commas' do
+      it 'should remove any commas from the number' do
+        expect(NumToWord.new('1,000').instance_variable_get(:@string_number)).to eq('1000')
+      end
+    end
+
+    context 'when number is very long' do
+      it 'should still work' do
+        expect(NumToWord.new('87,934,216').instance_variable_get(:@string_number)).to eq('87934216')
+      end
     end
   end
 
   describe 'NAMES' do
     it 'should return a hash of mappings' do
       expect(NumToWord::NAMES).to be_a(Hash)
+    end
+  end
+
+  describe '#to_string' do
+    it 'should return the number as human readable format' do
+      expect(NumToWord.new(87634592).to_string).to eq('eighty seven million six hundred thirty four thousand five hundred ninty two')
     end
   end
 end
